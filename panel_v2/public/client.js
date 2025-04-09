2ae8665975d7b7fcb96401b95da60b02c754f402
cuongtrinh69 <cuongtrinh6101@gmail.com>
2025-04-09 17:59:24 +0700

    Update client.js

--- panel_v2/public/client.js
+++ panel_v2/public/client.js
@@ -1,22 +1,26 @@
-const socket = io();
+<script src="/socket.io/socket.io.js"></script>
+<script>
+  const socket = io();
 
-const output = document.getElementById('output');
-const startBtn = document.getElementById('startBtn');
-const stopBtn = document.getElementById('stopBtn');
-const commandInput = document.getElementById('commandInput');
+  socket.on('stats', (data) => {
+    document.getElementById('uptime').innerText = data.uptime + 's';
+    document.getElementById('cpu').innerText = data.cpu + '%';
+    document.getElementById('ram').innerText = data.ram + '%';
+    document.getElementById('disk').innerText = data.disk + '%';
+  });
+</script>
 
-startBtn.onclick = () => {
-  const command = commandInput.value.trim();
-  if (command) {
-    socket.emit('start', { command });
-  }
-};
-
-stopBtn.onclick = () => {
-  socket.emit('stop');
-};
-
-socket.on('output', (data) => {
-  output.innerHTML += `<div>${data}</div>`;
-  output.scrollTop = output.scrollHeight;
-});
+<table>
+  <tr>
+    <td>Uptime</td><td id="uptime">...</td>
+  </tr>
+  <tr>
+    <td>CPU</td><td id="cpu">...</td>
+  </tr>
+  <tr>
+    <td>RAM</td><td id="ram">...</td>
+  </tr>
+  <tr>
+    <td>Disk</td><td id="disk">...</td>
+  </tr>
+</table>
