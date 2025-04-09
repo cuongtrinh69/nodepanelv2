fc0138639170ea1f0fc3ca5a490d3d9bf1d2690d
cuongtrinh69 <cuongtrinh6101@gmail.com>
2025-04-09 20:22:32 +0700

    Update client.js

--- panel_v2/public/client.js
+++ panel_v2/public/client.js
@@ -1,26 +1,37 @@
-<script src="/socket.io/socket.io.js"></script>
-<script>
-  const socket = io();
+const socket = io();
 
-  socket.on('stats', (data) => {
-    document.getElementById('uptime').innerText = data.uptime + 's';
-    document.getElementById('cpu').innerText = data.cpu + '%';
-    document.getElementById('ram').innerText = data.ram + '%';
-    document.getElementById('disk').innerText = data.disk + '%';
-  });
-</script>
+// Cập nhật thông số VPS (Uptime, CPU, RAM, Disk)
+socket.on('stats', (data) => {
+  document.getElementById('uptime').innerText = data.uptime + 's';
+  document.getElementById('cpu').innerText = data.cpu + '%';
+  document.getElementById('ram').innerText = data.ram + '%';
+  document.getElementById('disk').innerText = data.disk + '%';
+});
 
-<table>
-  <tr>
-    <td>Uptime</td><td id="uptime">...</td>
-  </tr>
-  <tr>
-    <td>CPU</td><td id="cpu">...</td>
-  </tr>
-  <tr>
-    <td>RAM</td><td id="ram">...</td>
-  </tr>
-  <tr>
-    <td>Disk</td><td id="disk">...</td>
-  </tr>
-</table>
+// Điều khiển các tab (Log, Code, Terminal)
+document.getElementById('logTabBtn').addEventListener('click', () => {
+  showTab('log');
+});
+document.getElementById('codeTabBtn').addEventListener('click', () => {
+  showTab('code');
+});
+document.getElementById('terminalTabBtn').addEventListener('click', () => {
+  showTab('terminal');
+});
+
+// Hàm hiển thị tab tương ứng
+function showTab(tabName) {
+  document.getElementById('logTab').style.display = (tabName === 'log') ? 'block' : 'none';
+  document.getElementById('codeTab').style.display = (tabName === 'code') ? 'block' : 'none';
+  document.getElementById('terminalTab').style.display = (tabName === 'terminal') ? 'block' : 'none';
+}
+
+// Xử lý nút Start/Stop (nếu bạn đã làm sẵn socket emit thì thêm ở đây)
+document.getElementById('startBtn').addEventListener('click', () => {
+  const cmd = document.getElementById('commandInput').value;
+  socket.emit('start', cmd);
+});
+
+document.getElementById('stopBtn').addEventListener('click', () => {
+  socket.emit('stop');
+});
